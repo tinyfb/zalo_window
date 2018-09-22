4e1159e1e51bbf8d998fc4dfa7ab187ae6a2ca41
Dương Đình Thiện <tinyfb.info@gmail.com>
2018-09-22 21:45:13 +0700

    crreate project

---
+++ README.md
@@ -0,0 +1,14 @@
+# tinyfb.windows
+TinyFB software version windows OS
+
+Phần mềm tự động tương tác và đăng tin Facebook TinyFB.
+
+## 1. Chạy phần mềm
+
+Để chạy phần mềm, click chuột vào file chay_chuong_trinh.bat hoặc tinyfb.bat và chờ chương trình tự động cập nhật và chạy.
+
+## 2. Quản lý phần mềm
+
+Bạn có thể trực tiếp sử dụng các chức năng trên phần mềm.
+
+Hoặc, sau khi có tài khoản bạn có thể sử dụng chức năng phần mềm thông qua website http://web.tinyfb.vn
---
+++ run.bat
@@ -0,0 +1,21 @@
+@echo off
+
+
+
+
+
+start jre\bin\javaw.exe -Dfile.encoding=UTF-8 -classpath .\jre\lib\charsets.jar;.\jre\lib\deploy.jar;.\jre\lib\ext\cldrdata.jar;.\jre\lib\ext\dnsns.jar;.\jre\lib\ext\jaccess.jar;.\jre\lib\ext\jfxrt.jar;.\jre\lib\ext\localedata.jar;.\jre\lib\ext\nashorn.jar;.\jre\lib\ext\sunec.jar;.\jre\lib\ext\sunjce_provider.jar;.\jre\lib\ext\sunpkcs11.jar;.\jre\lib\ext\zipfs.jar;.\jre\lib\javaws.jar;.\jre\lib\jce.jar;.\jre\lib\jfr.jar;.\jre\lib\jfxswt.jar;.\jre\lib\jsse.jar;.\jre\lib\management-agent.jar;.\jre\lib\plugin.jar;.\jre\lib\resources.jar;.\jre\lib\rt.jar;.\lib\ant-javafx.jar;.\lib\dt.jar;.\lib\javafx-mx.jar;.\lib\jconsole.jar;.\lib\packager.jar;.\lib\sa-jdi.jar;.\lib\tools.jar;.\libs\byte-buddy-1.8.15.jar;.\libs\client-combined-3.14.0-sources.jar;.\libs\client-combined-3.14.0.jar;.\libs\commons-codec-1.10.jar;.\libs\commons-exec-1.3.jar;.\libs\commons-logging-1.2-javadoc.jar;.\libs\commons-logging-1.2.jar;.\libs\gson-2.8.4.jar;.\libs\guava-25.0-jre.jar;.\libs\httpclient-4.5.5.jar;.\libs\httpcore-4.4.9.jar;.\libs\java-json.jar;.\libs\jsoup-1.11.3.jar;.\libs\log4j-1.2.17.jar;.\libs\okhttp-3.10.0.jar;.\libs\okio-1.14.1.jar;.\Zalo.jar Main
+
+
+
+goto end
+
+
+
+
+
+
+
+
+
+:end
